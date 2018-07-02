ddecff0c7ba1dd3a53eafb310cea3c66974752fb
zae <zhangzai1998@126.com>
2018-07-01 22:03:35 -0700

    def function is difficult, needs reviewing

---
+++ py_learn/def.py
@@ -0,0 +1,227 @@
+函数名其实就是指向一个函数对象的引用，完全可以把函数名赋给一个变量，相当于给这个函数起了一个“别名”：
+>>> a = abs # 变量a指向abs函数
+>>> a(-1) # 所以也可以通过a调用abs函数
+1
+
+#内置函数：数据类型转换
+>>> int('123')
+123
+>>> int(12.34)
+12
+>>> float('12.34')
+12.34
+>>> str(1.23)
+'1.23'
+>>> unicode(100)
+u'100'
+>>> bool(1)
+True
+
+自定义一个求绝对值的my_abs函数为例：
+
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+#空语句
+def nop():
+    pass
+但是：
+>>> my_abs('A')
+'A'
+当传入了不恰当的参数时，内置函数abs会检查出参数错误，而我们定义的my_abs没有参数检查，所以，这个函数定义不够完善。
+
+让我们修改一下my_abs的定义，对参数类型做检查，只允许整数和浮点数类型的参数。数据类型检查可以用内置函数isinstance实现：
+
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+import math
+
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+#python函数返回多个值
+import math语句表示导入math包，并允许后续代码引用math包里的sin、cos等函数。
+然后，我们就可以同时获得返回值：
+>>> x, y = move(100, 100, 60, math.pi / 6)
+>>> print(x, y)
+151.96152422706632 70.0
+
+但其实这只是一种假象，Python函数返回的仍然是单一值：
+>>> r = move(100, 100, 60, math.pi / 6)
+>>> print(r)
+(151.96152422706632, 70.0)
+原来返回值是一个tuple！但是，在语法上，返回一个tuple可以省略括号，而多个变量可以同时接收一个tuple，按位置赋给对应的值，所以，Python的函数返回多值其实就是返回一个tuple，但写起来更方便。
+
+函数执行完毕也没有return语句时，自动return None
+
+#函数的参数
+#一、默认参数
+def power(x, n=2):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+#二、可变参数
+默认参数很有用，但使用不当，也会掉坑里。默认参数有个最大的坑，演示如下：
+
+先定义一个函数，传入一个list，添加一个END再返回：
+
+def add_end(L=[]):
+    L.append('END')
+    return L
+
+当你正常调用时，结果似乎不错：
+
+>>> add_end([1, 2, 3])
+[1, 2, 3, 'END']
+>>> add_end(['x', 'y', 'z'])
+['x', 'y', 'z', 'END']
+当你使用默认参数调用时，一开始结果也是对的：
+
+>>> add_end()
+['END']
+但是，再次调用add_end()时，结果就不对了：
+
+>>> add_end()
+['END', 'END']
+>>> add_end()
+['END', 'END', 'END']
+
+很多初学者很疑惑，默认参数是[]，但是函数似乎每次都“记住了”上次添加了'END'后的list。
+
+原因解释如下：
+
+Python函数在定义的时候，默认参数L的值就被计算出来了，即[]，因为默认参数L也是一个变量，它指向对象[]，每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了，不再是函数定义时的[]了。
+
+!!! 定义默认参数要牢记一点：默认参数必须指向不变对象！
+要修改上面的例子，我们可以用None这个不变对象来实现：
+
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+现在，无论调用多少次，都不会有问题：
+
+>>> add_end()
+['END']
+>>> add_end()
+['END']
+
+#三、可变参数
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+定义可变参数和定义一个list或tuple参数相比，仅仅在参数前面加了一个*号。在函数内部，参数numbers接收到的是一个tuple，因此，函数代码完全不变。但是，调用该函数时，可以传入任意个参数，包括0个参数：
+
+>>> calc(1, 2)
+5
+>>> calc()
+0
+如果已经有一个list或者tuple，要调用一个可变参数怎么办？可以这样做：
+
+>>> nums = [1, 2, 3]
+>>> calc(nums[0], nums[1], nums[2])
+14
+这种写法当然是可行的，问题是太繁琐，所以Python允许你在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去：
+
+>>> nums = [1, 2, 3]
+>>> calc(*nums)
+14
+*nums表示把nums这个list的所有元素作为可变参数传进去。这种写法相当有用，而且很常见。
+
+#四、关键字参数
+可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict。请看示例：
+
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+函数person除了必选参数name和age外，还接受关键字参数kw。在调用该函数时，可以只传入必选参数：
+
+>>> person('Michael', 30)
+name: Michael age: 30 other: {}
+也可以传入任意个数的关键字参数：
+
+>>> person('Bob', 35, city='Beijing')
+name: Bob age: 35 other: {'city': 'Beijing'}
+>>> person('Adam', 45, gender='M', job='Engineer')
+name: Adam age: 45 other: {'gender': 'M', 'job': 'Engineer'}
+和可变参数类似，也可以先组装出一个dict，然后，把该dict转换为关键字参数传进去：
+
+>>> extra = {'city': 'Beijing', 'job': 'Engineer'}
+>>> person('Jack', 24, city=extra['city'], job=extra['job'])
+name: Jack age: 24 other: {'city': 'Beijing', 'job': 'Engineer'}
+当然，上面复杂的调用可以用简化的写法：
+
+>>> extra = {'city': 'Beijing', 'job': 'Engineer'}
+>>> person('Jack', 24, **extra)
+name: Jack age: 24 other: {'city': 'Beijing', 'job': 'Engineer'}
+**extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra。
+
+如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下：
+
+def person(name, age, *, city, job):
+    print(name, age, city, job)
+和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+
+调用方式如下：
+
+>>> person('Jack', 24, city='Beijing', job='Engineer')
+Jack 24 Beijing Engineer
+如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了：
+
+def person(name, age, *args, city, job):
+    print(name, age, args, city, job)
+命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错：
+
+>>> person('Jack', 24, 'Beijing', 'Engineer')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: person() takes 2 positional arguments but 4 were given
+由于调用时缺少参数名city和job，Python解释器把这4个参数均视为位置参数，但person()函数仅接受2个位置参数。
+
+命名关键字参数可以有缺省值，从而简化调用：
+
+def person(name, age, *, city='Beijing', job):
+    print(name, age, city, job)
+由于命名关键字参数city具有默认值，调用时，可不传入city参数：
+
+>>> person('Jack', 24, job='Engineer')
+Jack 24 Beijing Engineer
+
+参数组合
+在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
+
+#!!!小结
+Python的函数具有非常灵活的参数形态，既可以实现简单的调用，又可以传入非常复杂的参数。
+
+默认参数一定要用不可变对象，如果是可变对象，程序运行时会有逻辑错误！
+
+要注意定义可变参数和关键字参数的语法：
+
+*args是可变参数，args接收的是一个tuple；
+
+**kw是关键字参数，kw接收的是一个dict。
+
+以及调用函数时如何传入可变参数和关键字参数的语法：
+
+可变参数既可以直接传入：func(1, 2, 3)，又可以先组装list或tuple，再通过*args传入：func(*(1, 2, 3))；
+
+关键字参数既可以直接传入：func(a=1, b=2)，又可以先组装dict，再通过**kw传入：func(**{'a': 1, 'b': 2})。
+
+使用*args和**kw是Python的习惯写法，当然也可以用其他参数名，但最好使用习惯用法。
+
+命名的关键字参数是为了限制调用者可以传入的参数名，同时可以提供默认值。
+
+定义命名的关键字参数在没有可变参数的情况下不要忘了写分隔符*，否则定义的将是位置参数。
